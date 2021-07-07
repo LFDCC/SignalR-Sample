201c8468e033a77250f3fbcb81a34fb6781f7910
yanqili <1584329729@qq.com>
2021-07-07 11:02:15 +0800

    test dotnet publish to iis

--- SignalR-Sample-Vue/Startup.cs
+++ SignalR-Sample-Vue/Startup.cs
@@ -19,12 +19,14 @@ namespace SignalR_Sample_Vue
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +34,14 @@ namespace SignalR_Sample_Vue
             services.AddControllers();
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp";
+                if (Environment.IsDevelopment())
+                {
+                    configuration.RootPath = "ClientApp";
+                }
+                else
+                {
+                    configuration.RootPath = "ClientApp/dist";
+                }
             });
 
             services.AddSignalR();
